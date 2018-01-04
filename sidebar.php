b6fb7c6398721b87c4c9d315c81dff1f8597f1b9
YQC <i@yqc.im>
2018-01-05 00:32:33 +0800

    modify side post style

--- sidebar.php
+++ sidebar.php
@@ -34,6 +34,28 @@
     </div>
     <?php endif;?>
 
+    <?php if(!$this->is('index')):?>
+    <div class="blog-sidebar-widget blog-bor side-new-post">
+        <h2 class="blog-title"><span><?php _e('最新文章'); ?></span></h2>
+        <div class="am-list">
+            <?php $this->widget('Widget_Contents_Post_Recent', 'pageSize=6')->to($recent); ?>
+            <?php if ($recent->have()):?>
+            <?php while($recent->next()):?>
+            <div class="site-item">
+                <div class="side-pic">
+                    <img src="<?php echo  img_postthemb($recent,$this->options->themeUrl)?>">
+                </div>
+                <div class="side-con">
+                    <h2><a href="<?php $recent->permalink() ?>"><?php $recent->title() ?></a></h2>
+                    <div class="side-info"><?php $recent->date('F j, Y'); ?></div>
+                </div>
+            </div>
+            <?php endwhile;?>
+            <?php endif;?>
+        </div>
+    </div>
+    <?php endif;?>
+
     <div class="blog-clear-margin blog-sidebar-widget blog-bor am-g side-tag">
         <h2 class="blog-title"><span><?php _e('标签云'); ?></span></h2>
         <div class="am-u-sm-12 blog-clear-padding">
@@ -43,11 +65,5 @@
             <?php endwhile; ?>
         </div>
     </div>
-    <div class="blog-sidebar-widget blog-bor">
-        <h2 class="blog-title"><span><?php _e('最新文章'); ?></span></h2>
-        <ul class="am-list">
-            <?php $this->widget('Widget_Contents_Post_Recent')
-                ->parse('<li><a href="{permalink}">{title}</a></li>'); ?>
-        </ul>
-    </div>
+
 </div>
